31b2ec0bfa14b0ca4f5de7615976a38367860cd7
zagss <>
2023-09-22 01:12:30 +0800

    deploy

--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -39,7 +39,12 @@ func GetTags(c *gin.Context) {
 
 // AddTag 新增文章标签
 func AddTag(c *gin.Context) {
-
+	code := e.SUCCESS
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": "",
+	})
 }
 
 // EditTag 修改文章标签
